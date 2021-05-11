654deae53fe77797e7536a89fa56e7802b0e18a7
GitHubNaoki <70kio18.rf@gmail.com>
2021-05-11 14:58:42 +0900

    インデント修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
- describe 'ユーザー新規登録' do
-   it 'nicknameが空だと登録できない' do
-     user = User.new(nickname: '', email: 'kkk@gmail.com', password: '00000000', password_confirmation: '00000000')
-     expect(user.errors.full_messages).to include("Nickname can't be blank")
-   end
- end
+  describe 'ユーザー新規登録' do
+    it 'nicknameが空だと登録できない' do
+      user = User.new(nickname: '', email: 'kkk@gmail.com', password: '00000000', password_confirmation: '00000000')
+      expect(user.errors.full_messages).to include("Nickname can't be blank")
+    end
+  end
 end
