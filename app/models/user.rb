633091ffb6f85ae22074096ddfa5d986f965ed33
shuntas <shunta.suzuki@di-v.co.jp>
2021-05-12 16:43:12 +0900

    first

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,4 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :tweets
-  has_many :comments
-  validates :nickname, presence: true
 end
